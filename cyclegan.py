0ceac34a4798d468d4961f3ecd21efe7625538c3
Qixiang Chen <qc2666@gadi-login-06.gadi.nci.org.au>
2023-10-29 17:24:02 +1100

    update

--- cyclegan.py
+++ cyclegan.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     # ***************** hyper parameters *****************
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--batch_size", type=int, default=3)
+    parser.add_argument("--batch_size", type=int, default=12)
     parser.add_argument("--n_epoch", type=int, default=8)
     parser.add_argument("--lr", type=float, default=2e-4)
     parser.add_argument("--schedule", type=bool, default=False)
